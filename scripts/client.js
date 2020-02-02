4fb8c21aa13fb99cd51686063f73882f439bda5f
AwrenNuit <a.rennaker@yahoo.com>
2020-02-02 15:00:52 -0600

    reduced indentations, added regex test, greatly shortened switch case

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
     $(`#generate`).on(`click`, generateUniqueID);
 });
 
@@ -7,102 +7,83 @@ let charset = ``;
 let tempID = ``;
 
 function generateUniqueID(){
-    if($(`#length`).val() <= 50 && $(`#length`).val() >= 2){
-        updateCharsets();
-        createPassword();
-        appendPasswordToDom();
-        clearVariables();
-    }
-    else{
-        isLengthInRange();
-    }
+  if($(`#length`).val() <= 50 && $(`#length`).val() >= 2){
+    updateCharsets();
+    createPassword();
+    appendPasswordToDom();
+    clearVariables();
+  }
+  else{
+    isLengthInRange();
+  }
 }
 
 function appendPasswordToDom(){
-    let el = $(`#output`);
-    el.children().empty();
-    el.append(`<div>${tempID}</div>`);
+  let el = $(`#output`);
+  el.children().empty();
+  el.append(`<div>${tempID}</div>`);
 }
 
 function clearVariables(){
-    charset = ``;
-    tempID = ``;
-    length = 0;
+  charset = ``;
+  tempID = ``;
+  length = 0;
 }
 
 function createPassword(){
-    let randomCharset = charset.split(``);
+  let randomCharset = charset.split(``);
+  let notChar = /^[a-zA-Z0-9]/;
 
-    for(let i=0; tempID.length<length; i++) {
-        if(i == randomCharset.length){
-            i = 0;
-        }
-        else{
-            randomCharset.sort(() => Math.random() - 0.5);
-            if(checkDupe(randomCharset[i])){
-                tempID += randomCharset[i];
-            }
-        }
+  for(let i=0; tempID.length<length; i++) {
+    if(i == randomCharset.length){
+      i = 0;
     }
+    else{
+      randomCharset.sort(() => Math.random() - 0.5);
+      if(!randomCharset[i].match(notChar)){
+        if(checkDupe(randomCharset[i])){
+          tempID += randomCharset[i];
+        }
+      }
+      else{
+        tempID += randomCharset[i];
+      }
+    }
+  }
 }
 
 function isLengthInRange(){
-    $(`#length`).val() > 50 ? alert(`Max length is 50`) : '';
-    $(`#length`).val() < 2 ? alert(`Min length is 2`) : '';
+  $(`#length`).val() > 50 ? alert(`Max length is 50`) : '';
+  $(`#length`).val() < 2 ? alert(`Min length is 2`) : '';
 }
 
 function updateCharsets(){
-    length = $(`#length`).val();
-    charset = ``;
-    if($(`#up-alphabet`).is(`:checked`)){
-        charset += $(`#up-alphabet-display`).val();
-    }
-    if($(`#lo-alphabet`).is(`:checked`)){
-        charset += $(`#lo-alphabet-display`).val();
-    }
-    if($(`#numbers`).is(`:checked`)){
-        charset += $(`#numbers-display`).val();
-    }
-    if($(`#dash`).is(`:checked`)){
-        charset += $(`#dash-display`).val();
-    }
-    if($(`#symbols`).is(`:checked`)){
-        charset += $(`#symbols-display`).val();
-    }
+  length = $(`#length`).val();
+  charset = ``;
+  if($(`#up-alphabet`).is(`:checked`)){
+    charset += $(`#up-alphabet-display`).val();
+  }
+  if($(`#lo-alphabet`).is(`:checked`)){
+    charset += $(`#lo-alphabet-display`).val();
+  }
+  if($(`#numbers`).is(`:checked`)){
+    charset += $(`#numbers-display`).val();
+  }
+  if($(`#dash`).is(`:checked`)){
+    charset += $(`#dash-display`).val();
+  }
+  if($(`#symbols`).is(`:checked`)){
+    charset += $(`#symbols-display`).val();
+  }
 }
 
 function checkDupe(char){
-    switch(char){
-        case `-`:
-        case `_`:
-        case `#`:
-        case `;`:
-        case `:`:
-        case '\`':
-        case `~`:
-        case `!`:
-        case `@`:
-        case `#`:
-        case `$`:
-        case `%`:
-        case `^`:
-        case `&`:
-        case `*`:
-        case `(`:
-        case `)`:
-        case '+':
-        case `=`:
-        case `{`:
-        case `}`:
-        case `[`:
-        case `]`:
-        case `/`:
-        case `?`:
-        case `\\`:
-            if(tempID[tempID.length-1] == char){
-                return false;
-            }
-        default:
-            return true;
-    }
+  switch(char){
+    case char:
+      if(tempID[tempID.length-1] == char){
+        return false;
+      }
+    default:
+      return true;
+  }
 }
